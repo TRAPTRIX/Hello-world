2b25f5b01aa5af9f03d41db2eeea0453965dfea6
TRAPTRIX <52242023+TRAPTRIX@users.noreply.github.com>
2019-07-26 12:05:18 +0700

    Create dic.py

---
+++ SESSION10/dic.py
@@ -0,0 +1,12 @@
+# #items = {
+#     "name"="Nam Anh"
+#     "class"="11B5"
+#     "status"="single"
+# }
+
+
+# 1. create: items["Hobbies"] = "Soccer"
+# 2. update: items["status"] = "married"
+# 3. delete: del items["status"]
+
+Pay attention !!! : "="
