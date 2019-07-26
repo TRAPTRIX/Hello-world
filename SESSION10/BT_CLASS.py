b6662ba45c2b55bb52342b69616cb153fe654601
TRAPTRIX <52242023+TRAPTRIX@users.noreply.github.com>
2019-07-26 12:04:45 +0700

    Create BT_CLASS.py

---
+++ SESSION10/BT_CLASS.py
@@ -0,0 +1,68 @@
+# CREATE HOME.
+# Conan={
+#     "Author": "Aoyama GOSHO",
+#     "Main character": "Kudo Shinichi",
+#     "Content": input("Enter the content?")
+# }
+
+# print(Conan)
+
+
+
+
+# UPDATE HOME.
+# Conan={
+#     "Author": "Aoyama GOSHO",
+#     "Main character": "Kudo Shinichi",
+#     "Content": "Detective"
+# }
+
+# print(Conan)
+# while True:
+#     n=input("Enter your change by choosing one above? ")
+#     if n=="Author" or "Main character" or "Content":
+#         a=input("Your change? ")
+#         print(a)
+#     if n=="Author":
+#         Conan["Author"]= a
+#         print(Conan)
+#     elif n=="Main character":
+#         Conan["Main character"]= a
+#         print(Conan)
+#     elif n=="Content":
+#         Conan["Content"]= a
+#         print(Conan)
+
+
+
+
+# DELETE HOME.
+# Conan={
+#     "Author": "Aoyama GOSHO",
+#     "Main character": "Kudo Shinichi",
+#     "Content": "Detective"
+# }
+
+# print(Conan)
+# while True:
+#     n=input("Enter your change by choosing one above? ")
+#     if n=="Author":
+#         del Conan["Author"]
+#         print(Conan)
+#     elif n=="Main character":
+#         del Conan["Main character"]
+#         print(Conan)
+#     elif n=="Content":
+#         del Conan["Content"]
+#         print(Conan)
+
+
+
+
+
+
+
+
+
+
+    
