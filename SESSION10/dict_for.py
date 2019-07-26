a8d57d5a28c9aab7f2a577be72dfbc7786f150e9
TRAPTRIX <52242023+TRAPTRIX@users.noreply.github.com>
2019-07-26 12:06:00 +0700

    Create dict_for.py

---
+++ SESSION10/dict_for.py
@@ -0,0 +1,16 @@
+personal_information={
+    "name":"Nam Anh",
+    "age":"16",
+    "hobbies":"Nexus",
+}
+# for k in personal_information.keys():
+#     print(k)    
+#     print(k, personal_information[k])
+
+
+# for v in personal_information.values():
+#     print(v)
+
+
+for k,v in personal_information.items():
+    print(k,v)
