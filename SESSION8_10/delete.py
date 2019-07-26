6f2c89ef1ddffde4267164e9dbc86333921b89c6
TRAPTRIX <52242023+TRAPTRIX@users.noreply.github.com>
2019-07-26 12:10:56 +0700

    Create delete.py

---
+++ SESSION8_10/delete.py
@@ -0,0 +1,9 @@
+items = ["com","pho","chao","com rang"]
+#.pop: delete
+#1. Index-based: know the position(0,1,...).
+items.pop(1)
+print(items)
+
+#2. Value-based: know exactly the value(com,pho,...).
+items.remove("chao")
+print(items)
